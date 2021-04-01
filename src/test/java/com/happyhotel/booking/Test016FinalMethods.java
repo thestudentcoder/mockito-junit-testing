b72e7303ee544b211a1181b84e9378b17dbe8adb
Wayne Chan <wchan.it@outlook.com>
2021-04-01 15:56:58 -0500

    Mocking final methods

---
+++ src/test/java/com/happyhotel/booking/Test016FinalMethods.java
@@ -0,0 +1,67 @@
+package com.happyhotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class Test016FinalMethods {
+
+    private BookingService bookingService;
+
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setup() {
+        // create mocks for all 4 dependencies
+        this.paymentServiceMock = mock(PaymentService.class);
+        // mock will return an empty list
+        this.roomServiceMock = mock(RoomService.class);
+        this.bookingDAOMock = mock(BookingDAO.class);
+        this.mailSenderMock = mock(MailSender.class);
+
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+    }
+
+    @Test
+    void should_CountAvailablePlaces_When_OneRoomAvailable() {
+        // given
+        // whenever the roomServiceMock is used to call the getAvailableRooms method
+        // it should return a list with a single room in itself
+        when(this.roomServiceMock.getAvailableRooms())
+                .thenReturn(Collections.singletonList(new Room("Room 1", 2)));
+        int expected = 2;
+
+        // when
+        int actual = bookingService.getAvailablePlaceCount();
+
+        // then
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void should_CountAvailablePlaces_When_MultipleRoomsAvailable() {
+        // given
+        List<Room> rooms = Arrays.asList(new Room("Room 1", 2), new Room("Room 2", 5));
+        when(this.roomServiceMock.getAvailableRooms())
+                .thenReturn(rooms);
+        int expected = 7;
+
+        // when
+        int actual = bookingService.getAvailablePlaceCount();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+}
